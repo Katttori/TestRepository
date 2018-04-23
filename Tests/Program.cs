5866097e6847a6eb6f1418ddbcf9d2d1fffa401f
Nadiya Shvec <NadiyaYe.Shvec@livenau.net>
2018-04-23 16:14:49 +0300

    First commit

---
+++ Tests/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 600;
+            int b = 9;
+            byte c = checked((byte)(a + b));
+            Console.WriteLine($"{a}");
+        }
+    }
+}
